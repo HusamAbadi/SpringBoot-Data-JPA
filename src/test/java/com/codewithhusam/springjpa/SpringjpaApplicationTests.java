540b3c8dd48b42c9228c619609264f3583446e99
husam <husam@mail.com>
2024-05-25 20:22:23 +0200

    player configured

--- src/test/java/com/codewithhusam/springjpa/SpringjpaApplicationTests.java
+++ src/test/java/com/codewithhusam/springjpa/SpringjpaApplicationTests.java
@@ -1,13 +1,31 @@
 package com.codewithhusam.springjpa;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codewithhusam.model.Player;
+import com.codewithhusam.repo.PlayerRepository;
 
 @SpringBootTest
 class SpringjpaApplicationTests {
 
+	@Autowired
+	private PlayerRepository playerRepository;
+
 	@Test
-	void contextLoads() {
+	public void testSavePlayer() {
+		Player player = new Player();
+		player.setId(1L);
+		player.setName("Husam");
+		player.setScore(2);
+
+		playerRepository.save(player);
+
+		Player savedPlayer = playerRepository.findById(1L).get();
+		assertNotNull(savedPlayer);
 	}
 
 }
